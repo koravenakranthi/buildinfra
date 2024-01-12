63755760e59d439d6ccb3130ab62eb8d3dc443bd
koravenakranthi <kranthiksp14@outlook.com>
2024-01-12 23:28:19 +0530

    newcodekkk

--- main.tf
+++ main.tf
@@ -1,27 +1,22 @@
 terraform {
   required_providers {
-     azurerm = {
-    source = "hashicorp/azurerm"
-    version = "3.86.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.86.0"
+    }
+  }
+
+  required_version = ">= 1.6.6"
+}
+
+provider "azurerm" {
+  features {
+
   }
 }
-required_version = ">= 1.6.6"
-}
- provider "azurerm" {
-   features {
-     
-   }
- }
 
-resource "azurerm_resource_group" "sample" {
-  name     = "sample-resource-group"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "stosample" {
-  name                     = "samplestorageaccount"
-  resource_group_name      = azurerm_resource_group.sample.name
-  location                 = azurerm_resource_group.sample.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_resource_group" "resprod" {
+  count = length(var.resname)
+  name = "${var.resname[count.index]}"
+  location = var.region
 }
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "region" {
+    default = "East US"
+  
+}
+variable "resname" {
+    type = list(string)
+    default = ["roooog","soooog"]
+    
+}
