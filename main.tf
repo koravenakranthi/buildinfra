c6a023a37aa999219441f036e32dff297ae75cdc
koravenakranthi <kranthiksp14@outlook.com>
2024-01-12 18:44:15 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+     azurerm = {
+    source = "hashicorp/azurerm"
+    version = "3.86.0"
+  }
+}
+required_version = ">= 1.6.6"
+}
+ provider "azurerm" {
+   features {
+     
+   }
+ }
+
+resource azurerm_resource_group refrg {
+  name     = rg-resource-group
+  location = "East US"
+}
+
+resource azurerm_storage_account refsto {
+  name                     = torageaccount
+  resource_group_name      = azurerm_resource_group.refrg.name
+  location                 = azurerm_resource_group.refrg.location
+  account_tier             = Standard
+  account_replication_type = LRS
+}
